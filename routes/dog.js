5dcdf27b1ea9103327d1f5b093f33dff0f0d818c
mtircw <pt8dg6@gmail.com>
2024-06-12 08:24:20 +0900

    add dog api

--- routes/dog.js
+++ routes/dog.js
@@ -3,7 +3,7 @@ var router= express.Router();
 const request= require('request');
 
 router.get('/', async(req, res) =>{
-  request('https://qiita.com/api/v2/users/**', function(error, response, body) {
+  request('https://dog.ceo/api/breeds/image/random', function(error, response, body) {
     if (!error && response.statusCode == 200) {
       const data= JSON.parse(body);
       res.json(data);
